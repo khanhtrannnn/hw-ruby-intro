97941cf0eb1a610651b0a9ea1c838b1deb4eab31
Khanh Tran <khanhtrannnn@gmail.com>
2016-05-05 15:03:52 +0000

    Done Part2 and Fix Error Part3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -23,18 +23,24 @@ def sum_to_n? arr, n
   else arr.permutation(2).any? { |i, j| i + j == n }
   end
 end
+
 # Part 2
 
 def hello(name)
-  return "Hello, " + name
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.downcase!
+  return (/[a-z]/=~s[0])!= nil && (/[ueoai]/=~s[0]) == nil ? true : false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s=~/^[01]+$/ 
+  	return Integer(s) % 4 == 0 ? true : false
+  else
+  	return false
+  end
 end
 
 # Part 3
@@ -49,23 +55,11 @@ class BookInStock
 		@price = price
 	end
 
-	def isbn
-		@isbn
-	end
-	
-	def isbn=(new_isbn)
-		@isbn = new_isbn
-	end
-
-	def price
-		@price
-	end
-
-	def price=(new_price)
-		@price = new_price
-	end
+	#auto set and get
+ 	attr_accessor :isbn
+  attr_accessor :price
 
 	def price_as_string
-		return number_to_currency(@price, precision: 2) 
+		 "$"+'%.2f' % ((@price * 100).round / 100.0)
 	end
 end
